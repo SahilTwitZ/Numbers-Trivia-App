392ddebee98af92b398b2dcfdefc67fad7e3d97f
Shahil <sahil321nis@gmail.com>
2025-01-27 22:51:41 +0530

    first commit

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let getFactBtn = document.getElementById("get-fact-btn");
 let ranFactBtn = document.getElementById("get-rand-fact-btn");
 let fact = document.querySelector(".fact");
-let url = "http://numbersapi.com/";
+let url = "https://numbersapi.com/";
 
 let fetchFact = (num) => {
   let finalUrl = url + num;
